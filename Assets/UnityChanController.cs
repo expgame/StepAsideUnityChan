4f85b6534e5114f61279617455120886e05ad2ea
expgame <exprobot@gmail.com>
2018-06-27 22:11:46 +0900

    パーティクルを作る前まで。

--- Assets/UnityChanController.cs
+++ Assets/UnityChanController.cs
@@ -16,6 +16,11 @@ public class UnityChanController : MonoBehaviour {
 	private float upForce = 500.0f;
 	//左右の移動できる範囲（追加）
 	private float movableRange = 3.4f;
+	//動きを減速させる係数（追加）
+	private float coefficient = 0.95f;
+
+	//ゲーム終了の判定（追加）
+	private bool isEnd = false;
 
 
 	// Use this for initialization
@@ -32,6 +37,14 @@ public class UnityChanController : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+		
+		//ゲーム終了ならUnityちゃんの動きを減衰する（追加）
+		if (this.isEnd) {
+			this.forwardForce *= this.coefficient;
+			this.turnForce *= this.coefficient;
+			this.upForce *= this.coefficient;
+			this.myAnimator.speed *= this.coefficient;
+		}
 
 		//Unityちゃんに前方向の力を加える
 		this.myRigidbody.AddForce (this.transform.forward * this.forwardForce);
@@ -45,17 +58,39 @@ public class UnityChanController : MonoBehaviour {
 			this.myRigidbody.AddForce (this.turnForce, 0, 0);
 		}
 
-			//Jumpステートの場合はJumpにfalseをセットする（追加）
-			if (this.myAnimator.GetCurrentAnimatorStateInfo(0).IsName ("Jump")) {
-				this.myAnimator.SetBool ("Jump", false);
-			}
+		//Jumpステートの場合はJumpにfalseをセットする（追加）
+		if (this.myAnimator.GetCurrentAnimatorStateInfo (0).IsName ("Jump")) {
+			this.myAnimator.SetBool ("Jump", false);
+		}
 
-			//ジャンプしていない時にスペースが押されたらジャンプする（追加）
-			if (Input.GetKeyDown(KeyCode.Space) && this.transform.position.y < 0.5f) {
-				//ジャンプアニメを再生（追加）
-				this.myAnimator.SetBool ("Jump", true);
-				//Unityちゃんに上方向の力を加える（追加）
-				this.myRigidbody.AddForce (this.transform.up * this.upForce);
-			}
+		//ジャンプしていない時にスペースが押されたらジャンプする（追加）
+		if (Input.GetKeyDown (KeyCode.Space) && this.transform.position.y < 0.5f) {
+			//ジャンプアニメを再生（追加）
+			this.myAnimator.SetBool ("Jump", true);
+			//Unityちゃんに上方向の力を加える（追加）
+			this.myRigidbody.AddForce (this.transform.up * this.upForce);
+		}
+
 	}
+
+			//トリガーモードで他のオブジェクトと接触した場合の処理（追加）
+			void OnTriggerEnter(Collider other) {
+
+				//障害物に衝突した場合（追加）
+				if (other.gameObject.tag == "CarTag" || other.gameObject.tag == "TrafficConeTag") {
+					this.isEnd = true;
+				}
+
+				//ゴール地点に到達した場合（追加）
+				if (other.gameObject.tag == "GoalTag") {
+					this.isEnd = true;
+				}  
+				//コインに衝突した場合（追加）
+				if (other.gameObject.tag == "CoinTag") {
+				//接触したコインのオブジェクトを破棄（追加）
+				Destroy (other.gameObject);
+			}
+	
+		}
+
 }
